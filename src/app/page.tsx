0aafa959cb7379cf029360061fcc41223fbc317e
Eduardo Gomes <edugomesf@icloud.com>
2023-11-02 14:33:25 -0300

    feat: start home page with responsive title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
 export default function Home() {
   return (
-    <h1>Password generator</h1>
+    <main
+      className="w-[343px] mt-16 mx-auto md:w-[540px]"
+    >
+      <h1 className="text-base font-bold text-grey text-center md:text-2xl">
+        Password generator
+      </h1>
+
+
+    </main>
   )
 }
